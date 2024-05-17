588522c03c491ec98fea8c1041449cd1443489df
Dietmar E <86421371+Dima0687@users.noreply.github.com>
2024-05-17 05:02:14 +0200

    create folder structure for mono server (#8)
    * Dima: created folder structure for monolithic server, a bit of schema.sql and some initial properties
    
    * Dima: added readme file on local repo

---
+++ src/main/java/com/finalprojectgrp5/sma/Application.java
@@ -0,0 +1,13 @@
+package com.finalprojectgrp5.sma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
